96d0e88001176464b2ecc5bc80561bb693424a66
gfederica <effegiudice@gmail.com>
2021-06-09 20:02:15 +0200

    inizio sezione album

---
+++ src/components/Album.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+    <img :src="item.poster" alt="" class="img-fluid">
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Album',
+  props: ["item"]
+}
+</script>
+
+<style scoped lang="scss">
+
+@import "../style/general.scss";
+@import "../style/mixins.scss";
+
+    img {
+      height: 100px;
+    }
+</style>
--- src/components/MusicList.vue
+++ src/components/MusicList.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="container-fluid">
-
+      <div class="row">
+        <div class="col" v-for="(album,index) in albums" :key="index">
+            <Album :item="album"/>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import Album from './Album.vue';
+import axios from 'axios';
 
+export default {
+name: 'MusicList',
+components: {
+    Album
+},
+data: function() {
+    return {
+        apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+        albums: []
+    }
+},
+created: function() {
+    axios
+    .get (this.apiUrl)
+    .then (
+        (response) => {
+            this.albums = response.data.response;
+        }
+    )
+}
 }
 </script>
 
