c091304f5d41b75d7268df7d24c6dfb7f1f25678
gfederica <effegiudice@gmail.com>
2021-06-09 16:55:56 +0200

    Iniziato layout

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <img src="../assets/spotify.png" alt="logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+
+<style scoped lang="scss">
+@import "../style/general.scss";
+@import "../style/mixins.scss";
+
+  header {
+    height: 10vh;
+    background-color: $lightGrey;
+    @include flex-vertical-only;
+
+    img {
+      height: 80%;
+      margin-left: 10px;
+    }
+  }
+</style>
---
+++ src/components/MusicList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-fluid">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+@import "../style/general.scss";
+@import "../style/mixins.scss";
+
+    .container-fluid {
+        height: 90vh;
+        background-color: $darkGrey;
+
+    }
+</style>
